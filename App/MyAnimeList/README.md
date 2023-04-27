6e95efbc8d6d56c91b24c895f7bd06e9e8be909b
Maharjan Binish <binish@fenrir.co.jp>
2023-04-27 21:03:33 +0900

    added workspace and package

---
+++ App/MyAnimeList/README.md
@@ -0,0 +1,3 @@
+# MyAnimeList
+
+A description of this package.
---
+++ App/MyAnimeList/Tests/MyAnimeListTests/MyAnimeListTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MyAnimeList
+
+final class MyAnimeListTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MyAnimeList().text, "Hello, World!")
+    }
+}
