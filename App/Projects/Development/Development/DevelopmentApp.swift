ee3707367e1aa31ba4d535c59b20941065f7215b
Maharjan Binish <binish@fenrir.co.jp>
2023-04-27 21:06:36 +0900

    added development project

---
+++ App/Projects/Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by Maharjan Binish on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
